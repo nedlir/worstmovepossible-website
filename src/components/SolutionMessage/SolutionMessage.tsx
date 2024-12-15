5825eba32833b7da8fcc602b79da5efb6ff444a5
nedlir <neliran2@telem.openu.ac.il>
2024-12-15 22:44:58 +0100

    for now a good and responsive design

--- src/components/SolutionMessage/SolutionMessage.tsx
+++ src/components/SolutionMessage/SolutionMessage.tsx
@@ -6,13 +6,11 @@ type SolutionMessageProps = {
 
 const SolutionMessage: React.FC<SolutionMessageProps> = ({ description }) => (
   <div className="solution-message">
-    <div className="solution-header">
-      <svg viewBox="0 0 24 24" width="24" height="24">
-        <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" />
-      </svg>
-      <h3>Puzzle Solved!</h3>
-    </div>
-    <p>{description}</p>
+    <svg viewBox="0 0 24 24" width="24" height="24">
+      <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" />
+    </svg>
+    <h3>Puzzle Solved!</h3>
+    {description}
   </div>
 );
 
